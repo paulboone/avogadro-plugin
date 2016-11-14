0479fcf4ccd8e331fe465ef9aef4f66b72238406
Paul Boone <narcissus.pong@gmail.com>
2016-11-14 11:57:46 -0500

    handle maximum bounds on bonds

--- avogadro_plugin/utils.py
+++ avogadro_plugin/utils.py
@@ -1,3 +1,6 @@
+rcov = [0, 0.38, 0.32, 1.34, 0.90, 0.82, 0.77, 0.75, 0.73, 0.71, 0.69, 1.54, 1.30, 1.18, 1.11]
+
+
 
 def extrapolate_bonds(atoms):
     bonds = []
@@ -7,7 +10,8 @@ def extrapolate_bonds(atoms):
                         (atoms[j][1] - atoms[i][1])**2 +
                         (atoms[j][2] - atoms[i][2])**2 ) ** 0.5
 
-            if distance >= 0.16:
+            max_bond_distance = (rcov[atoms[i][3]] + rcov[atoms[j][3]] + 0.45)
+            if distance >= 0.16 and distance <= max_bond_distance:
                 bonds.append((i,j))
 
     return bonds
