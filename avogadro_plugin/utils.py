c0cea950d7ef279164f9e3650a95463e851bf93c
Paul Boone <narcissus.pong@gmail.com>
2016-11-15 10:38:28 -0500

    add util methods for parsing atoms and bonds from cjson + tests

--- avogadro_plugin/utils.py
+++ avogadro_plugin/utils.py
@@ -54,3 +54,17 @@ def extrapolate_bonds(atoms):
                 bonds.append((atoms_z[i][0],atoms_z[j][0]))
 
     return bonds
+
+
+def cjson_get_atoms(cjson):
+    coords_json = cjson["cjson"]["atoms"]["coords"]["3d"]
+    elements = cjson["cjson"]["atoms"]["elements"]["number"]
+
+    coords = [ (*coords_json[i*3:(i+1)*3], elements[i]) for i in range(0, int(len(coords_json) / 3)) ]
+    return coords
+
+def cjson_get_bonds(cjson):
+    bonds_json = cjson["cjson"]["bonds"]["connections"]["index"]
+
+    bonds = [ frozenset(bonds_json[i*2:(i+1)*2]) for i in range(0, int(len(bonds_json) / 2)) ]
+    return bonds
