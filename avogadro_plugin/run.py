75f7b420861f9d4d8d5ab6992edc1749e5d492e4
Paul Boone <narcissus.pong@gmail.com>
2017-01-17 14:43:42 -0500

    invert python helper plugin so that it is imported and called by the plugin script; use argparse instead of click

--- avogadro_plugin/run.py
+++ avogadro_plugin/run.py
@@ -1,52 +1,39 @@
-import importlib
+
+import argparse
 import json
-import os
 import sys
 
-import click
+def avogadro_plugin_call(method_name, run_workflow_method, print_options_method, menu_path):
+    parser = argparse.ArgumentParser('Scale molecular coordinates.')
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--print-options', action='store_true')
+    parser.add_argument('--run-workflow', action='store_true')
+    parser.add_argument('--display-name', action='store_true')
+    parser.add_argument('--menu-path', action='store_true')
+    args = vars(parser.parse_args())
 
-@click.command()
-@click.argument('command_module')
-@click.option('--print-dialog/--run-transformation', default=False)
-@click.option('--language', default='en.us', help="preferred language to use")
-def run(command_module, print_dialog, language):
-    """
-    This command expects to recieve a JSON file via stdin that contains the structure selected in
-    avogadro, along with any parameters from the dialog. You can call this method without flags, in
-    which case it will attempt to run the transformation, or with the flag --print-dialog, which
-    instead returns the JSON necessary via stdout to create a dialog to set the parameters of the
-    transformation.
+    debug = args['debug']
+    status = None
 
-    A note to people using this as a template:
-
-    This method handles the comand line interface, unmarshals the data from JSON, and passes the
-    data to either get_dialog_options() or run_transformation(), depending on the flags. Upon
-    completion, it marshals the results back to JSON and returns it to avogadro via stdout.
-
-    In general, you shouldn't need to modify this method. Instead, make changes to
-    get_dialog_options() and run_transformation().
-    """
-    input_stream = click.get_text_stream('stdin').read()
-    input_json = json.loads(input_stream)
-    structure = input_json['cjson']
-
-
-    # since this will be run from the plugin's directory, add the current directory to the path
-    sys.path.append(os.getcwd())
-    plugin = __import__(command_module)
-    status = 0
-    if print_dialog:
+    if args['display_name']:
+        results = {"display_name": method_name}
+    elif args['menu_path']:
+        results = {"menu_path": menu_path}
+    elif args['print_options']:
+        input_json = json.loads(sys.stdin.read())
         if 'options' in input_json:
             options = input_json['options']
         else:
             options = {}
-        results = plugin.get_dialog_options(structure, options)
-    else:
-        options = input_json['options']
-        status, results = plugin.run_transformation(structure, options)
+        results = print_options_method(input_json['cjson'], options)
+    elif args['run_workflow']:
+        input_json = json.loads(sys.stdin.read())
+        if 'options' in input_json:
+            options = input_json['options']
+        else:
+            options = {}
+        status, results = run_workflow_method(input_json['cjson'], options)
 
-    # marshal to json and return across stdout
     sys.stdout.write(json.dumps(results))
-
     if status:
         sys.exit(status)
