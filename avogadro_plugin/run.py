18edc3ac47a6757d3c2f446d1755c2da516f046c
Paul Boone <narcissus.pong@gmail.com>
2017-01-17 15:13:56 -0500

    refactor stdin to method for resuse; still outstanding decision on how the options structure will get passed

--- avogadro_plugin/run.py
+++ avogadro_plugin/run.py
@@ -3,6 +3,15 @@ import argparse
 import json
 import sys
 
+def process_stdin():
+    input_json = json.loads(sys.stdin.read())
+    if 'options' in input_json:
+        # options = {option['key']:option for option in input_json['options']}
+        options = input_json['options']
+    else:
+        options = {}
+    return (input_json['cjson'], options)
+
 def avogadro_plugin_call(method_name, run_workflow_method, print_options_method, menu_path):
     parser = argparse.ArgumentParser('Scale molecular coordinates.')
     parser.add_argument('--debug', action='store_true')
@@ -20,19 +29,11 @@ def avogadro_plugin_call(method_name, run_workflow_method, print_options_method,
     elif args['menu_path']:
         results = {"menu_path": menu_path}
     elif args['print_options']:
-        input_json = json.loads(sys.stdin.read())
-        if 'options' in input_json:
-            options = input_json['options']
-        else:
-            options = {}
-        results = print_options_method(input_json['cjson'], options)
+        input_json, options = process_stdin()
+        results = print_options_method(input_json, options)
     elif args['run_workflow']:
-        input_json = json.loads(sys.stdin.read())
-        if 'options' in input_json:
-            options = input_json['options']
-        else:
-            options = {}
-        status, results = run_workflow_method(input_json['cjson'], options)
+        input_json, options = process_stdin()
+        status, results = run_workflow_method(input_json, options)
 
     sys.stdout.write(json.dumps(results))
     if status:
